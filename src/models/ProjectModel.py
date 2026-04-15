daa51721c4950f2b380e0d88814e6f0e71b58d4c
Aya-Hamdy12 <aya585359@gmail.com>
2026-04-15 20:39:14 +0200

    set indexes in db for assets

--- src/models/ProjectModel.py
+++ src/models/ProjectModel.py
@@ -1,12 +1,11 @@
 from .BaseDataModel import BaseDataModel
 from .db_schemes import Project
 from .enums.DataBaseEnum import DataBaseEnum
-from bson.objectid import ObjectId
-
 
 class ProjectModel(BaseDataModel):
+
     def __init__(self, db_client: object):
-        super().__init__(db_client)
+        super().__init__(db_client=db_client)
         self.collection = self.db_client[DataBaseEnum.COLLECTION_PROJECT_NAME.value]
 
 
@@ -20,12 +19,11 @@ class ProjectModel(BaseDataModel):
     async def init_collection(self):
         all_collections = await self.db_client.list_collection_names()
         if DataBaseEnum.COLLECTION_PROJECT_NAME.value not in all_collections:
-            await self.db_client.create_collection(DataBaseEnum.COLLECTION_PROJECT_NAME.value)
-            # Create indexes
+            self.collection = self.db_client[DataBaseEnum.COLLECTION_PROJECT_NAME.value]
             indexes = Project.get_indexes()
             for index in indexes:
                 await self.collection.create_index(
-                    keys=index["key"],
+                    index["key"],
                     name=index["name"],
                     unique=index["unique"]
                 )
@@ -34,28 +32,28 @@ class ProjectModel(BaseDataModel):
 
     async def create_project(self, project: Project):
 
-        project_dict = project.model_dump(by_alias=True, exclude_none=True)
-        
-        result = await self.collection.insert_one(project_dict)
-        project_id = str(result.inserted_id)
-        return project_id
+        result = await self.collection.insert_one(project.dict(by_alias=True, exclude_unset=True))
+        project.id = result.inserted_id
+
+        return project
 
     async def get_project_or_create_one(self, project_id: str):
-        record = await self.collection.find_one({"project_id": project_id})
-        
+
+        record = await self.collection.find_one({
+            "project_id": project_id
+        })
+
         if record is None:
             # create new project
-            new_project = Project(project_id=project_id)
-            
-            inserted_id = await self.create_project(project=new_project)
-            
-            new_project.id = ObjectId(inserted_id)
-            
-            return new_project
-            
+            project = Project(project_id=project_id)
+            project = await self.create_project(project=project)
+
+            return project
+        
         return Project(**record)
 
-    async def get_all_projects(self, page: int = 1, page_size: int = 10):
+    async def get_all_projects(self, page: int=1, page_size: int=10):
+
         # count total number of documents
         total_documents = await self.collection.count_documents({})
 
@@ -63,17 +61,13 @@ class ProjectModel(BaseDataModel):
         total_pages = total_documents // page_size
         if total_documents % page_size > 0:
             total_pages += 1
-        
-        # Pagination using skip and limit
-        cursor = self.collection.find().skip((page - 1) * page_size).limit(page_size)
-        
+
+        cursor = self.collection.find().skip( (page-1) * page_size ).limit(page_size)
         projects = []
         async for document in cursor:
-            projects.append(Project(**document))
+            projects.append(
+                Project(**document)
+            )
 
         return projects, total_pages
     
-
-
-
-
--- src/routes/data.py
+++ src/routes/data.py
@@ -9,7 +9,9 @@ import logging
 from .schemes.data import ProcessRequest
 from models.ProjectModel import ProjectModel
 from models.ChunkModel import ChunkModel
-from models.db_schemes import DataChunk
+from models.AssetModel import AssetModel
+from models.db_schemes import DataChunk, Asset
+from models.enums.AssetTypeEnum import AssetTypeEnum
 
 
 
@@ -55,16 +57,24 @@ async def upload_data(request: Request, project_id: str, file: UploadFile, app_s
             status_code=status.HTTP_400_BAD_REQUEST, 
             content={
                 "message": f"{ResponseSignal.FILE_UPLOAD_FAILED.value} Error: {str(e)}"}
-            )        
+            )
+    # Store the assets into database
+    asset_model = await AssetModel.create_instance(db_client=request.app.db_client)
+    asset_resource = Asset(
+        asset_project_id=project.id,
+        asset_type=AssetTypeEnum.FILE.value,
+        asset_name=file_id,
+        asset_size=os.path.getsize(file_path)
+    )
+
+    asset_record = await asset_model.create_asset(asset=asset_resource)
 
     return JSONResponse(
         status_code=status.HTTP_200_OK, 
         content={"message": ResponseSignal.FILE_UPLOAD_SUCCESS.value,
-                  "file_id": file_id,
+                  "file_id": str(asset_record.id),
                 }
         )
-
-
 
 
 
@@ -89,7 +99,7 @@ async def process_endpoint(request: Request, project_id: str, process_request: P
             content={"message": ResponseSignal.PROCESSING_FAILED.value}
         )
 
-
+    
     file_chunks = process_controller.process_file_content(
         file_content=file_content, 
         file_id=file_id, 
@@ -135,7 +145,7 @@ async def process_endpoint(request: Request, project_id: str, process_request: P
                     "content": chunk.page_content,
                     "metadata": chunk.metadata
                 }
-                for idx, chunk in enumerate(file_chunks)
+                for idx, chunk in enumerate(file_chunks[:3]) # Return only the first 3 chunks for preview
             ]
         }
     )
